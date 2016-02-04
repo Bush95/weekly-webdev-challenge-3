ff0759135eead5da39d7477232d3a063ab07a770
Bush95 <adi139597@gmail.com>
2016-02-04 16:16:25 +0100

    fix to menu, added welcome section

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
+var is_mobile = $('.js_trigger').css('visibility') == 'visible';
+
 $(".js_trigger, .menu__link").on('click',function(){
     if ($('.js_trigger').css('visibility') == 'visible')
         $('.js_menu').toggle('fast');
+});
+
+$('.menu__link').on('click', function(event){
+    if (is_mobile){ 
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top - $(".menu__header").height()
+        }, 500);
+        return false;
+    }
+    else {
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 500);
+        return false;
+    }
+    
 });
